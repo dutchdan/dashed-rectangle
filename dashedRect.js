cae9f1c3585d3107beaf19172f3637ba5c86ed81
dutchdan <daniel_freese@hotmail.com>
2018-05-15 16:32:03 +0100

    top and bottom and left  and right in same loop

--- dashedRect.js
+++ dashedRect.js
@@ -1,34 +1,20 @@
 dashRect = function (x, y, w, h, d) {
-  //draw top
+  //draw top and bottom
   for (let i = 0; i < w; i += d * 2) {
     let end = x + i + d;
     if (end > w) {
       end = w;
     }
     line(i + x, y, end, y);
+    line(i + x, h, end, h);
   }
-  //draw right
+  //draw sides
   for (let i = 0; i < h; i += d * 2) {
     let end = i + y + d;
     if (end > h) {
       end = h;
     }
     line(w, i + y, w, end);
-  }
-  //draw bottom
-  for (let i = 0; i < w; i += d * 2) {
-    let end = i + x + d;
-    if (end > w) {
-      end = w;
-    }
-    line(i + x, h, end, h);
-  }
-  //draw left
-  for (let i = 0; i < h; i += d * 2) {
-    let end = i + x + d;
-    if (end > h) {
-      end = h;
-    }
     line(x, i + y, x, end);
   }
 }
